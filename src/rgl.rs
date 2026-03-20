8691655f75e7a886c9ba65add10df2e127d03a29
Mark-Prime <msmarkieer@gmail.com>
2026-03-20 12:57:02 -0500

    added the ability to save aliases to a cfg

--- src/rgl.rs
+++ src/rgl.rs
@@ -1,5 +1,9 @@
+use std::{fs::{File, read_to_string}};
+
 use serde_json::{ json, Value };
 use reqwest;
+
+use crate::settings::load_settings;
 
 pub fn get_users(steam_ids: Vec<String>) -> Value {
   let mut users: Value = json!({});
@@ -32,4 +36,46 @@ pub fn get_users(steam_ids: Vec<String>) -> Value {
   }
 
   users
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
+
+pub fn save_users_to_cfg(users: Value) {
+  let settings = load_settings();
+
+  if settings["features"]["demo_scanner"]["rgl_force_rename"].as_bool().unwrap() == false {
+    return;
+  }
+
+  let file_path = format!("{}\\cfg\\mls_rgl_aliases.cfg", settings["tf_folder"].as_str().unwrap());
+  
+  let mut lines: Vec<String> = match File::open(&file_path) {
+    Ok(_) => read_lines(file_path.as_str()),
+    Err(_) => vec![],
+  };
+  
+  use std::io::Write;
+
+  for user_id in users.as_object().unwrap().keys() {
+    if user_id == "response" {
+      continue;
+    }
+
+    let name = &users[user_id]["name"];
+    let line = format!("ce_playeraliases_add {} {};", user_id, name);
+
+    if !lines.contains(&line) {
+      lines.push(line);
+    }
+  }
+
+  let mut cfg_file = File::create(&file_path).unwrap();
+
+  cfg_file.write_all(lines.join("\n").as_bytes()).unwrap();
 }
